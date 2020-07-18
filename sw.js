9562cac58fb9bbea65a192dac0a609217b75f1b9
AJ Speller <ajspeller@gmail.com>
2020-07-17 22:47:27 -0400

    cache versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCachename = 'sites-static';
+const staticCachename = 'sites-static-v2';
 const assets = [
   '/',
   '/index.html',
@@ -24,6 +24,15 @@ self.addEventListener('install', (evt) => {
 
 self.addEventListener('activate', (evt) => {
   // console.log('server work has been activated');
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCachename)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (evt) => {
