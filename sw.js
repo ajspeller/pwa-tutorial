2c166856539394eb8417dd0bf034b90967c290e5
AJ Speller <ajspeller@gmail.com>
2020-07-17 21:06:20 -0400

    Cache assets

--- sw.js
+++ sw.js
@@ -1,11 +1,30 @@
+const staticCachename = 'sites-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 self.addEventListener('install', (evt) => {
-  console.log('server worker has been installed');
+  // console.log('server worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCachename).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('server work has been activated');
+  // console.log('server work has been activated');
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
