a9a5ca67076ab3da0715f479a83a3c32189e7c1a
AJ Speller <ajspeller@gmail.com>
2020-07-17 20:28:44 -0400

    add fetch events

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", (evt) => {
-  console.log("server worker has been installed");
+self.addEventListener('install', (evt) => {
+  console.log('server worker has been installed');
 });
 
-self.addEventListener("activate", (evt) => {
-  console.log("server work has been activated");
+self.addEventListener('activate', (evt) => {
+  console.log('server work has been activated');
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('fetch event', evt);
 });
