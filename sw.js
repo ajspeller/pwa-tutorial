d9bb1d2ef745eb49cffcc1d8e0afbbdde6c31734
AJ Speller <ajspeller@gmail.com>
2020-07-17 20:14:01 -0400

    Add activate event

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", (evt) => {
   console.log("server worker has been installed");
 });
+
+self.addEventListener("activate", (evt) => {
+  console.log("server work has been activated");
+});
